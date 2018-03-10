89dbab9a1f6bea6900d2d2d0ba921b6d6a0b959f
Dan Sanche <sanche@google.com>
2018-03-09 17:14:11 -0800

    added return statements

--- container_registry/container_analysis/src/sample/samples.go
+++ container_registry/container_analysis/src/sample/samples.go
@@ -7,42 +7,36 @@ import (
 	containeranalysispb "google.golang.org/genproto/googleapis/devtools/containeranalysis/v1alpha1"
 )
 
-func deleteOccurrence(occurrenceName string){
+func deleteOccurrence(occurrenceName string) (error){
 	ctx := context.Background()
 	c, err := containeranalysis.NewClient(ctx)
 	if err != nil {
-		// TODO: Handle error.
-		fmt.Println(err)
+		return err
 	}
 	req := &containeranalysispb.DeleteOccurrenceRequest{Name: occurrenceName}
-	err = c.DeleteOccurrence(ctx, req)
-	if err != nil {
-		// TODO: Handle error.
-		fmt.Println(err)
-	}
+	return c.DeleteOccurrence(ctx, req)
 }
 
-func createNote(noteId, projectId string){
+
+func createNote(noteId, projectId string) (*containeranalysispb.Note, error) {
 	ctx := context.Background()
 	c, err := containeranalysis.NewClient(ctx)
 	if err != nil {
-		// TODO: Handle error.
-		fmt.Println(err)
+		return nil, err
 	}
 	parent := containeranalysis.ProjectPath(projectId)
 	noteVulType := containeranalysispb.VulnerabilityType{}
 	noteType := containeranalysispb.Note_VulnerabilityType{&noteVulType}
 	note := containeranalysispb.Note{NoteType:&noteType}
 	req := &containeranalysispb.CreateNoteRequest{Parent:parent, NoteId:noteId, Note:&note}
-	_, err = c.CreateNote(ctx, req)
-	if err != nil {
-		// TODO: Handle error.
-		fmt.Println(err)
-	}
+	return c.CreateNote(ctx, req)
 }
 
 
 func main() {
 	fmt.Println("hello world")
-	createNote("sadfadsfasdadsfasd", "sanche-testing-project")
+	_, err := createNote("test3", "sanche-testing-project")
+	if err != nil {
+		fmt.Println(err)
+	}
 }

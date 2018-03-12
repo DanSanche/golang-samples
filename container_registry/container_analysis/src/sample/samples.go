06a9b32829911a34db6f00591d4d47310ea09471
Dan Sanche <sanche@google.com>
2018-03-12 13:01:51 -0700

    export all sample functions

--- container_registry/container_analysis/src/sample/samples.go
+++ container_registry/container_analysis/src/sample/samples.go
@@ -9,7 +9,7 @@ import (
 
 // [START create_note]
 //Creates and returns a new note
-func createNote(noteId, projectId string) (*containeranalysispb.Note, error) {
+func CreateNote(noteId, projectId string) (*containeranalysispb.Note, error) {
 	ctx := context.Background()
 	c, err := containeranalysis.NewClient(ctx)
 	if err != nil {
@@ -26,36 +26,36 @@ func createNote(noteId, projectId string) (*containeranalysispb.Note, error) {
 
 // [START create_occurrence]
 //Creates and returns a new occurrence
-func createOccurrence(imageUrl, parentNoteId, projectId string) (*containeranalysispb.Occurrence, error){
+func CreateOccurrence(imageUrl, parentNoteId, projectId string) (*containeranalysispb.Occurrence, error){
 	return nil, nil
 }
 // [END create_occurrence]
 
 // [START update_note]
 //Makes an update to an existing note
-func updateNote(updated *containeranalysispb.Note, noteId, projectId string) (error){
+func UpdateNote(updated *containeranalysispb.Note, noteId, projectId string) (error){
 	return nil
 }
 // [END update_note]
 
 // [START update_occurrence]
 //Makes an update to an existing occurrence
-func updateOccurrence(updated *containeranalysispb.Occurrence, occurrenceName string) (error){
+func UpdateOccurrence(updated *containeranalysispb.Occurrence, occurrenceName string) (error){
 	return nil
 }
 // [END update_occurrence]
 
 
-// [START delete_node]
+// [START delete_note]
 //Deletes an existing note from the project
-func deleteNote(noteId, projectId string) (error){
+func DeleteNote(noteId, projectId string) (error){
 	return nil
 }
-// [END delete_node]
+// [END delete_note]
 
 // [START delete_occurrence]
 //Deletes an existing occurrence from the project
-func deleteOccurrence(occurrenceName string) (error){
+func DeleteOccurrence(occurrenceName string) (error){
 	ctx := context.Background()
 	c, err := containeranalysis.NewClient(ctx)
 	if err != nil {
@@ -66,16 +66,16 @@ func deleteOccurrence(occurrenceName string) (error){
 }
 // [END delete_occurrence]
 
-// [START get_node]
+// [START get_note]
 //Retrieves a note based on its noteId and projectId
-func getNote(noteId, projectId string) (*containeranalysispb.Note, error){
+func GetNote(noteId, projectId string) (*containeranalysispb.Note, error){
 	return nil, nil
 }
-// [END get_node]
+// [END get_note]
 
 // [START get_occurrence]
 //Retrieves an occurrence based on its name
-func getOccurrence(occurrenceName string) (*containeranalysispb.Occurrence, error){
+func GetOccurrence(occurrenceName string) (*containeranalysispb.Occurrence, error){
 	return nil, nil
 }
 // [END get_occurrence]
@@ -83,41 +83,41 @@ func getOccurrence(occurrenceName string) (*containeranalysispb.Occurrence, erro
 // [START discovery_info]
 //Prints the Discovery occurrence created for a specified image
 //This occurrence contains information about the initial scan on the image
-func getDiscoveryInfo(imageUrl, projectId string) (error){
+func GetDiscoveryInfo(imageUrl, projectId string) (error){
 	return nil
 }
 // [END discovery_info]
 
 // [START occurrences_for_note]
 //Retrieves all the occurrences associated with a specified note
-func getOccurrencesForNote(noteId, projectId string) (int, error){
+func GetOccurrencesForNote(noteId, projectId string) (int, error){
 	return 0, nil
 }
 // [END occurrences_for_note]
 
 // [START occurrences_for_image]
 //Retrieves all the occurrences associated with a specified image
-func getOccurrencesForImage(imageUrl, projectId string) (int, error){
+func GetOccurrencesForImage(imageUrl, projectId string) (int, error){
 	return 0, nil
 }
 // [END occurrences_for_image]
 
 // [START pubsub]
 //Handle incoming occurrences using a pubsub subscription
-func pubsub(subscriptionId string, timeout int, projectId string) (int, error){
+func Pubsub(subscriptionId string, timeout int, projectId string) (int, error){
 	return 0, nil
 }
 
 //Creates and returns a pubsub subscription listening to the occurrence topic.
 //This topic provides updates when occurrences are modified
-func createOccurrenceSubscription(subscriptionId, projectId string) (error){
+func CreateOccurrenceSubscription(subscriptionId, projectId string) (error){
 	return nil
 }
 // [END pubsub]
 
 func main() {
 	fmt.Println("hello world")
-	_, err := createNote("test3", "sanche-testing-project")
+	_, err := CreateNote("test3", "sanche-testing-project")
 	if err != nil {
 		fmt.Println(err)
 	}
